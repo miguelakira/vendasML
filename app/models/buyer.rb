036aa584e6abf8afa9747b134f36680f42845d1a
Miguel Akira Ykeuti Meiga <miguel.akira@gmail.com>
2011-08-16 17:09:30 -0300

    feitas validacoes e associacoes entre buyers e sales

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,2 +1,5 @@
 class Buyer < ActiveRecord::Base
+	validates :name, :nickname, :email, :sale_id, :presence => true
+	belongs_to :sale
+    accepts_nested_attributes_for :sale
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,4 @@
 class Sale < ActiveRecord::Base
+	validates :title, :price, :tax, :cost, :presence => true
+	has_many :buyers
 end
