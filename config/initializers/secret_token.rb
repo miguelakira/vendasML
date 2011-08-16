fd4f0116475054aa4dae356c2148e1e5f10dcb12
unknown <Miguel@.(none)>
2011-08-16 15:10:09 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vendasml::Application.config.secret_token = '76b3b8cf54398fb4cbc35c8888e8b869dfdabc95019479fc23e630eaab52bdebb2f89135f746a967c6172184367778c812aad07c4e6a71caae2e46624ee46b8e'
