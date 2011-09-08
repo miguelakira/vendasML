e17968dc9137b44260aa32f1b716e32523cc7933
Miguel Akira Ykeuti Meiga <miguel.akira@gmail.com>
2011-09-08 10:27:08 -0300

    link para visualizar tracking code

--- app/helpers/buyers_helper.rb
+++ app/helpers/buyers_helper.rb
@@ -1,3 +1,9 @@
 module BuyersHelper
-	
+
+	def view_tracking(code)
+		url = "http://websro.correios.com.br/sro_bin/txect01$.QueryList?P_LINGUA=001&P_TIPO=001&P_COD_UNI=#{ code }"
+		link_to code, url, :target => '_blank'
+	end
+
+
 end
